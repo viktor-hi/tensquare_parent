3c0365641ed15a52eb9e09c5c6bc17b677388dca
viktor-hi <411206167@qq.com>
2020-01-14 00:49:19 +0800

    idea提交

---
+++ springboot-project/src/test/java/cn/chen/springbootproject/SpringbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.chen.springbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
