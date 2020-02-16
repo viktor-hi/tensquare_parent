b740b7e1d19e15d6cf90d4334a0257aa75f01e3c
viktor-hi <411206167@qq.com>
2020-02-17 01:39:25 +0800

    2.17 更新消息通知系统

--- tensquare_parent/tensquare_notice/src/main/java/com/tensquare/notice/controller/NoticeController.java
+++ tensquare_parent/tensquare_notice/src/main/java/com/tensquare/notice/controller/NoticeController.java
@@ -1,8 +1,11 @@
 package com.tensquare.notice.controller;
 
+import com.baomidou.mybatisplus.plugins.Page;
+import com.tensquare.entity.PageResult;
 import com.tensquare.entity.Result;
 import com.tensquare.entity.StatusCode;
 import com.tensquare.notice.pojo.Notice;
+import com.tensquare.notice.pojo.NoticeFresh;
 import com.tensquare.notice.service.NoticeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -28,6 +31,27 @@ public class NoticeController {
     public Result add(@RequestBody Notice notice) {
         noticeService.add(notice);
         return new Result(true, StatusCode.OK, "新增成功");
+    }
+
+    @RequestMapping(value = "/findPage/{page}/{size}", method = RequestMethod.GET)
+    public Result findPage(@PathVariable int page,@PathVariable int size){
+        Page<Notice> pagelist = noticeService.findPage(page,size);
+        PageResult<Notice> noticePageResult = new PageResult<>(pagelist.getTotal(), pagelist.getRecords());
+        return new Result(true, StatusCode.OK, "查询成功",noticePageResult);
+    }
+
+    @RequestMapping(value = "/{id}",method = RequestMethod.PUT)
+    public Result update(@PathVariable String id,@RequestBody Notice notice){
+        notice.setId(id);
+        noticeService.update(notice);
+        return new Result(true,StatusCode.OK,"修改成功");
+    }
+    /*根据用户id查询该用户的待推送消息*/
+    @RequestMapping(value = "/freshPage/{userId}/{page}/{size}",method = RequestMethod.GET)
+    public Result freshPage(@PathVariable int userId,@PathVariable int page,@PathVariable int size){
+        Page<NoticeFresh> pagelist = noticeService.freshPage(userId,page,size);
+        PageResult<NoticeFresh> noticePageResult = new PageResult<>(pagelist.getTotal(), pagelist.getRecords());
+        return new Result(true,StatusCode.OK,"查询成功",noticePageResult);
     }
 }
 
--- tensquare_parent/tensquare_notice/src/main/java/com/tensquare/notice/service/NoticeService.java
+++ tensquare_parent/tensquare_notice/src/main/java/com/tensquare/notice/service/NoticeService.java
@@ -1,5 +1,7 @@
 package com.tensquare.notice.service;
 
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.plugins.Page;
 import com.tensquare.notice.dao.NoticeDao;
 import com.tensquare.notice.dao.NoticeFreshDao;
 import com.tensquare.notice.pojo.Notice;
@@ -9,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * @author haixin
@@ -19,15 +22,15 @@ public class NoticeService {
 
     @Autowired
     private NoticeDao noticeDao;
-    public Notice findById(String id) {
-        return noticeDao.selectById(id);
-    }
-
     @Autowired
     private IdWorker idWorker;
 
     @Autowired
     private NoticeFreshDao noticeFreshDao;
+    public Notice findById(String id) {
+        return noticeDao.selectById(id);
+    }
+
 
     public void add(Notice notice) {
         //1.设置初始值
@@ -42,5 +45,25 @@ public class NoticeService {
         noticeFresh.setNoticeId(id);//消息id
         noticeFresh.setUserId(notice.getReceiverId());//待通知用户的id
         noticeFreshDao.insert(noticeFresh);
+    }
+
+    public Page<Notice> findPage(int page, int size) {
+        Page<Notice> pagelist = new Page<>(page, size);
+        List<Notice> notices = noticeDao.selectPage(pagelist, new EntityWrapper<Notice>());
+        pagelist.setRecords(notices);
+        return pagelist;
+    }
+
+    public void update(Notice notice) {
+        noticeDao.updateById(notice);
+    }
+
+    public Page<NoticeFresh> freshPage(int userId, int page, int size) {
+        Page<NoticeFresh> pagelist = new Page<>(page, size);
+        EntityWrapper<NoticeFresh> objectEntityWrapper = new EntityWrapper<>();
+        objectEntityWrapper.eq("userId",userId);
+        List<NoticeFresh> noticeFreshes = noticeFreshDao.selectPage(pagelist, objectEntityWrapper);
+        pagelist.setRecords(noticeFreshes);
+        return pagelist;
     }
 }
