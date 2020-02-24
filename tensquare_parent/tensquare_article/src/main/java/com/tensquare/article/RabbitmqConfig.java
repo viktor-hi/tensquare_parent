83c5925562fafdf9bf472c830ca7d781fd1a2cea
viktor-hi <411206167@qq.com>
2020-02-24 20:56:49 +0800

    2.24 更新文章模块消息订阅功能，配置netty

--- tensquare_parent/tensquare_article/src/main/java/com/tensquare/article/RabbitmqConfig.java
+++ tensquare_parent/tensquare_article/src/main/java/com/tensquare/article/RabbitmqConfig.java
@@ -12,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
+
 
 /**
  * Rabbitmq配置
